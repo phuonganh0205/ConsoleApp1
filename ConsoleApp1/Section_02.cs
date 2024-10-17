2fd89a35ad99efc3050de2ab3a367347edd05636
phuonganh0205 <anhdang.31231027548@st.ueh.edu.vn>
2024-10-18 00:07:21 +0700

    Section_02.cs

--- ConsoleApp1/Section_02.cs
+++ ConsoleApp1/Section_02.cs
@@ -1,71 +1,124 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DangNguyenPhuongAnh_31231027548
+using System.Diagnostics.CodeAnalysis;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+namespace DangBinhAn_31231021006
 {
-    internal class Section_02
+    internal class Section_2
     {
         public static void Main()
         {
+            //Question1();
+            //Question2();
+            //Question3();
+            //Question4();
+            //Question5();
+            //Question6();
+            //Question7();
+            //Question8();
+            //Question9();
+            Question10();
 
-            Question_03();
-            Question_04();
-            Console.WriteLine("Press any key to stop");
+            Console.WriteLine("press any key to escape");
             Console.ReadKey();
-
         }
-        /// <summary>
-        /// . to Add / Sum Two Numbers.
-        /// </summary>
-        public static void Question_01()
+        public static void Question1()
         {
-            Console.Write("Enter a number a = ");
+            Console.Write("Moi nhap a: ");
             int a = int.Parse(Console.ReadLine());
-            Console.Write("Enter a number b = ");
+            Console.Write("Moi nhap b: ");
             int b = int.Parse(Console.ReadLine());
-            int sum = a + b;
-            int product = a * b;
-            Console.WriteLine($"{a} + {b} = {sum}");
-            Console.WriteLine($"{a} * {b} = {product}");
-        }
-        /// <summary>
-        /// . to Swap Values of Two Variables.
-        /// </summary>
-        public static void Question_02()
-        {
-            int a = 10, b = 20;
-            Console.WriteLine("So a va b truoc khi doi la: a ={0}, b = {1}", a, b);
-            (a, b) = (b, a);
-            Console.WriteLine("So a va b sau khi doi la: a={0}, b = {1}", a, b);
+            int tong = a + b;
+            int tich = a * b;
+            Console.WriteLine($"{a} + {b} = {tong}");
+            Console.WriteLine($"{a} * {b} = {tich}");
             Console.ReadKey();
         }
-    }
-    /// <summary>
-    /// . to Multiply two Floating Point Numbers.
-    /// </summary>
-    public static void Question_03()
-    {
-        float a = 10.15f;
-        float b = 12.45f;
-        Console.WriteLine("So a la: a = {0}", a);
-        Console.WriteLine("So b la: b = {0}", b);
-        float kq = a * b;
-        Console.WriteLine($"{a} * {b} = {kq}");
+        public static void Question2()
+        {
+            int a = 15, b = 16;
+            Console.WriteLine("So a va b truoc khi doi la: a = {0}, b = {1}",a,b);
+            (a, b) = (b, a);
+            Console.WriteLine("So a va b sau khi doi la: a = {0}, b = {1}",a,b);
+            Console.ReadKey();
+        }
+        public static void Question3()
+        {
+            float a = 10.15f;
+            float b = 15.25f;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            float kq = a * b;
+            Console.WriteLine("a * b = " + kq);
+            Console.ReadKey();
+        }
+        public static void Question4()
+        {
+            Console.WriteLine("Nhap feet: ");
+            float feet = float.Parse(Console.ReadLine());
+            double doi = feet * 0.3048;
+            Console.WriteLine("{0} feet = {1} meters", feet, doi);
+            Console.ReadKey();
+        }
+        public static void Question5()
+        {
+            Console.Write("nhap do C: ");
+            float C = float.Parse(Console.ReadLine());
+            float doi = (C * 9 / 5) + 32;
+            Console.WriteLine($"{C} do C = {doi} do F");
+            Console.Write("nhap do F: ");
+            float F = float.Parse(Console.ReadLine());
+            float doi1 = (F - 32) * 5 / 9;
+            Console.WriteLine($"{F} do F = {doi1} do C");
+            Console.ReadKey();
+        }
+        public static void Question6()
+        {
+            Console.WriteLine("Size of int: " + sizeof(int));
+            Console.WriteLine("Size of float: " + sizeof(float));
+            Console.WriteLine("Size of double: " + sizeof(double));
+            Console.WriteLine("Size of deciaml: " + sizeof(decimal));
+            Console.WriteLine("Size of long: " + sizeof(long));
+            Console.WriteLine("Size of short: " + sizeof(short));
+
+        }
+        public static void Question7()
+        {
+            Console.WriteLine("Input the character: ");
+            char character = char.Parse(Console.ReadLine());
+            int ASCII = (int)character;
+            Console.WriteLine("ASCII value: " + ASCII);
+
+        }
+        public static void Question8()
+        {
+            Console.Write("Nhap ban kinh (r): ");
+            float r = float.Parse(Console.ReadLine());
+            double S = Math.Pow(r, 2) * Math.PI;
+            Console.WriteLine("Dien tich duong tron: " + S);
+            Console.ReadKey();
+        }
+        public static void Question9()
+        {
+            Console.Write("Nhap canh hinh vuong: ");
+            float a = float.Parse(Console.ReadLine());
+            float S = a * a;
+            Console.WriteLine("Dien tich hinh vuong la: " + S);
+            Console.ReadKey();
+        }
+        public static void Question10()
+        {
+            Console.Write("Nhap so ngay: ");
+            int numbDays = int.Parse(Console.ReadLine());
+            int year = numbDays / 365;
+            int week = (numbDays % 365) / 7;
+            int day = (numbDays % 365) % 7;
+            Console.WriteLine($"kq: {year} nam {week} tuan {day} ngay ");
+        }
 
     }
-    /// <summary>
-    /// . to convert feet to meter.
-    /// </summary>
-    public static void Question_04()
 }
-    public static void Question_10()
-    (//365 = 1 y 1 w 3 da
-     int days = int.Parse(Console.ReadLine());
-int years = days / 365 
-int 
 
 
 
